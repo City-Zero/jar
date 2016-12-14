6d418706fd9d8899b55bfd913cb0c4b597455f3c
City-Zero <liyutong19961231@gmail.com>
2016-12-14 22:43:34 +0800

    2016年12月14日

---
+++ Input&OutputStream/t19_3.java
@@ -0,0 +1,23 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
+/**
+ * Created by lyt on 16-12-14.
+ */
+public class t19_3 {
+    public static void main(String []args){
+        try {
+            DataInputStream input = new DataInputStream(new FileInputStream("E19_3.dat"));
+            int s,n;
+            s = 0;
+            while((n = (int)input.read()) != -1){
+                System.out.println(n);
+                s += n;
+            }
+            input.close();
+            System.out.println(s);
+        }catch(Exception ex){
+            System.err.println(ex);
+        }
+    }
+}
